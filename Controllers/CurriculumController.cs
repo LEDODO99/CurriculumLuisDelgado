865ad7218b7ba8e1beb0ac106dbac4e308358814
Luis Delgado <ledodo99@hotmial.com>
2018-02-07 07:48:01 -0600

    Hacerlo bonito

---
+++ Controllers/CurriculumController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using netCore.Models;
+
+namespace netCore.Controllers
+{
+    public class CurriculumController : Controller
+    {
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
